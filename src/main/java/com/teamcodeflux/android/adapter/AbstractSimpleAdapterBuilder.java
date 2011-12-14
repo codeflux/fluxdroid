130e83ff9849811aafbf752833a9afeb504b1226
Ealden Esto E. Escanan <ealden@teamcodeflux.com>
2011-12-14 12:14:04 +0800

    Helper superclass for SimpleAdapterBuilder
    This exposes abstract methods and constructors that aids the building
    of SimpleAdapter.  Constructor requires an Android Context, as well as
    a List of classes that the specific builder builds on for SimpleAdapter.
    the resource, from, and to are then exposed as abstract methods.
    
    Subclasses of AbstractSimpleAdapterBuilder could now encapsulate all
    steps necessary to build a SimpleAdapter for type T.
    
    To use, given a sample class DataModel, a DataModelAdapterBuilder class
    should be created, and all abstract methods and constructors implemented:
    
        public DataModelAdapterBuilder extends AbstactSimpleAdapterBuilder<DataModel> {
            public DataModelAdapter(final Context context, List<DataModel> data) {
                super(context, data);
            }
    
            protected List<? extends Map<String, ?>> data(List<DataModel> data) {
                ...
            }
    
            protected int resource() {
                ...
            }
    
            protected String[] from() {
                ...
            }
    
            protected int[] to() {
                ...
            }
        }
    
    This could then be used in this manner:
    
        List<DataModel> data = new ArrayList<DataModel>();
        data.add(...);
        ...
    
        SimpleAdapter adapter = new DataModelAdapterBuilder(this, data).build();
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@teamcodeflux.com>

---
+++ src/main/java/com/teamcodeflux/android/adapter/AbstractSimpleAdapterBuilder.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2011 CodeFlux, Inc <info@teamcodeflux.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.teamcodeflux.android.adapter;
+
+import android.content.Context;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class AbstractSimpleAdapterBuilder<T> implements SimpleAdapterBuilder {
+    private Context context;
+    private List<T> data;
+
+    public AbstractSimpleAdapterBuilder(final Context context, final List<T> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public SimpleAdapter build() {
+        return new SimpleAdapter(context, data(data), resource(), from(), to());
+    }
+
+    protected abstract List<? extends Map<String, ?>> data(List<T> data);
+    protected abstract int resource();
+    protected abstract String[] from();
+    protected abstract int[] to();
+}
