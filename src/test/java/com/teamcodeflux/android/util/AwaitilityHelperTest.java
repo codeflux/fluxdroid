3baed087188943a33338c276f4f971b8051d8682
Ealden Esto E. Escanan <ealden@teamcodeflux.com>
2011-12-15 02:48:02 +0800

    Helper method for Awaitility
    This creates Callable instances based on the text of TextViews for
    use with Awaitility.  Usage would thus be:
    
        await().until(AwaitilityHelper.notEmpty(textView);
    
    Note taht notEmpty() is the initial helper method, and more helper
    methods are expected in the future.
    
    Signed-off-by: Ealden Esto E. Escanan <ealden@teamcodeflux.com>

---
+++ src/test/java/com/teamcodeflux/android/util/AwaitilityHelperTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011 CodeFlux, Inc <info@teamcodeflux.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.teamcodeflux.android.util;
+
+import android.widget.TextView;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.teamcodeflux.android.util.AwaitilityHelper.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.*;
+
+public class AwaitilityHelperTest {
+    private TextView textView;
+
+    @Before
+    public void setUp() {
+        textView = mock(TextView.class);
+    }
+
+    @Test
+    public void shouldReturnTrueIfTextViewIsNotEmpty() throws Exception {
+        given(textView.getText()).willReturn("Hello, world!");
+
+        assertThat(notBlank(textView).call(), equalTo(true));
+    }
+
+    @Test
+    public void shouldReturnFalseIfTextViewIsBlank() throws Exception {
+        given(textView.getText()).willReturn("");
+
+        assertThat(notBlank(textView).call(), equalTo(false));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionIfTextViewIsNull() throws Exception {
+        // TextView should not be null, so we throw an IllegalArgumentException instead of checking if null
+        given(textView.getText()).willReturn(null);
+
+        assertThat(notBlank(textView).call(), equalTo(false));
+    }
+}
