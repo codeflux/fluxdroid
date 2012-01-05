f21d0a17a6b64d2be7b325adf3f5dc2e331e8659
Jhun Mag-atas <jhun@teamcodeflux.com>
2012-01-05 18:10:49 +0800

    Added map to temporarily hold key-gesture pairs.
    This makes the build() method build the whole GestureLibrary which is more proper than the previous implementation.
    
    Signed-off-by: Jhun Mag-atas <jhun@teamcodeflux.com>

--- src/main/java/com/teamcodeflux/android/util/GestureLibraryBuilder.java
+++ src/main/java/com/teamcodeflux/android/util/GestureLibraryBuilder.java
@@ -3,11 +3,29 @@ package com.teamcodeflux.android.util;
 import android.gesture.Gesture;
 import android.gesture.GestureLibrary;
 
-public final class GestureLibraryBuilder implements Builder<GestureLibrary>{
-    private GestureLibrary gestureLibrary;
+import java.util.HashMap;
+import java.util.Map;
 
-    public GestureLibraryBuilder() {
-        gestureLibrary = new GestureLibrary() {
+public final class GestureLibraryBuilder implements Builder<GestureLibrary>{
+    private Map<String, Gesture> gestureMap;
+
+    private GestureLibraryBuilder() {
+        gestureMap = new HashMap<String, Gesture>();
+    }
+
+    public static GestureLibraryBuilder newInstance() {
+        return new GestureLibraryBuilder();
+    }
+
+    public GestureLibraryBuilder addGesture(String key, Gesture gesture) {
+        this.gestureMap.put(key, gesture);
+
+        return this;
+    }
+
+    @Override
+    public GestureLibrary build() {
+        GestureLibrary gestureLibrary = new GestureLibrary() {
             @Override
             public boolean save() {
                 return true;
@@ -18,20 +36,11 @@ public final class GestureLibraryBuilder implements Builder<GestureLibrary>{
                 return true;
             }
         };
-    }
 
-    public static GestureLibraryBuilder newInstance() {
-        return new GestureLibraryBuilder();
-    }
+        for (String key : gestureMap.keySet()) {
+            gestureLibrary.addGesture(key, gestureMap.get(key));
+        }
 
-    public GestureLibraryBuilder addGesture(String key, Gesture gesture) {
-        this.gestureLibrary.addGesture(key, gesture);
-
-        return this;
-    }
-
-    @Override
-    public GestureLibrary build() {
-        return this.gestureLibrary;
+        return gestureLibrary;
     }
 }
